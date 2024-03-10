65f070b589702e7a56293265fc6a53353d6dfd86
YangQX <1370655310@qq.com>
2024-03-10 19:21:31 +0800

    maven配置初步完成

--- src/main/java/org/nap/diabuddy_companion_server/DiabuddyCompanionServerApplication.java
+++ src/main/java/org/nap/diabuddy_companion_server/DiabuddyCompanionServerApplication.java
@@ -1,13 +1,16 @@
 package org.nap.diabuddy_companion_server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class DiabuddyCompanionServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiabuddyCompanionServerApplication.class, args);
+        log.info("项目启动成功>>>>>");
     }
 
 }
