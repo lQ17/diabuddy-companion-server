d8995c647ad058eb19d26961710f90e53a32dd30
YangQX <1370655310@qq.com>
2024-03-10 17:14:17 +0800

    创建以及测试git

---
+++ src/main/java/org/nap/diabuddy_companion_server/DiabuddyCompanionServerApplication.java
@@ -0,0 +1,13 @@
+package org.nap.diabuddy_companion_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabuddyCompanionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiabuddyCompanionServerApplication.class, args);
+    }
+
+}
